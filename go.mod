09ad63ab8ed1f9dbf3eb4ac8a66d6556dbb0492e
CheukChiu <cheuk0.0chiu@gmail.com>
2019-12-11 17:00:18 +0800

    feat(v1): add v1 example

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/easyopsapis/openapi-go-example
+
+go 1.12
+
+require (
+	github.com/easyopsapis/easyops-api-go v0.0.0-20191209101223-418346e1f22f
+	github.com/easyopsapis/openapi-go v0.0.0-20191211041410-d0fa7ad73e76
+	github.com/mwitkow/go-proto-validators v0.2.0 // indirect
+)
---
+++ v1/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	cmdb "github.com/easyopsapis/easyops-api-go/protorepo-cmdb"
+	"github.com/easyopsapis/easyops-api-go/protorepo-cmdb/instance"
+	"github.com/easyopsapis/openapi-go/gerr"
+	"github.com/easyopsapis/openapi-go/v1"
+	"os"
+)
+
+func main() {
+	// OpenAPI服务地址: 192.168.100.162:8109
+	// accessKey: c92f21653163ec91d238129c
+	// secretKey: 66555962666442714948516f4c70504c4c79686d544a795172766f73506c5967
+	client, _ := openapi.NewClient("192.168.100.162:8109", "c92f21653163ec91d238129c", "66555962666442714948516f4c70504c4c79686d544a795172766f73506c5967")
+
+	// 根据服务名封装client, 注入cmdb client中
+	c := cmdb.NewClient(openapi.WrapClient("cmdb", client))
+
+	// 利用 cmdb client 查询实例详情
+	d, err := c.Instance.GetDetail(context.Background(), &instance.GetDetailRequest{
+		ObjectId:   "APP",
+		InstanceId: "582f3323eef19",
+		Fields:     "name",
+	})
+
+	if err != nil {
+		// 当请求返回错误时, 利用 gerr 包从 err 中提取 Status 信息 ( 包含错误码, 错误信息等信息 )
+		status := gerr.FromError(err)
+		fmt.Println(status.Code(), status.Message())
+		os.Exit(1)
+	}
+
+	fmt.Println(d.Fields["name"].GetStringValue())
+}
